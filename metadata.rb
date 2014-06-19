33cac4095733a92bb2bf9feb4daf51d831c3dad5
kevTheDev <kev.j.edwards@gmail.com>
2014-06-19 10:59:33 +0100

    Added name to metadata

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
+name 'pdftk'
 maintainer       "Heavy Water Software Inc."
-maintainer_email "aaron@slyness.org"
+maintainer_email "kev.j.edwards@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures pdftk"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
-version          "0.0.1"
+version          "0.0.2"
